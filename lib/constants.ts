8033ae502128d5f069bb0cae7e714fae1b6ef834
Mohit Attri <attrimohit6846@gmail.com>
2025-12-28 09:47:07 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,61 @@
+export type EventItem = {
+    title: string;
+    image: string;
+    slug: string;
+    time: string;
+    location: string;
+    date: string;
+}
+
+
+export const events: EventItem[] = [
+    {
+        title: 'Hackathon 2024',
+        image: '/images/event1.png',
+        slug: 'hackathon-2024',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+    {
+        title: 'Hackathon 2023',
+        image: '/images/event2.png',
+        slug: 'hackathon-2023',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+     {
+        title: 'Hackathon 2025',
+        image: '/images/event3.png',
+        slug: 'hackathon-2025',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+     {
+        title: 'Hackathon 2020',
+        image: '/images/event4.png',
+        slug: 'hackathon-2020',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+    {
+        title: 'Hackathon 2019',
+        image: '/images/event5.png',
+        slug: 'hackathon-2019',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+    {
+        title: 'Hackathon 2018',
+        image: '/images/event6.png',
+        slug: 'hackathon-2018',
+        time: '10:00 AM - 6:00 PM',
+        location: 'New York, NY',
+        date: '2024-09-15',
+    },
+
+]
